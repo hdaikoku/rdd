337600da305559f0ff725e57ab9ca96ca3ae20ec
Harunobu Daikoku <daikoku@hpcs.cs.tsukuba.ac.jp>
2015-11-18 15:30:52 +0900

    get slaves configuration, the target textfile path & mapreduce shared library from command-line arguments

--- master/master.cc
+++ master/master.cc
@@ -5,44 +5,68 @@
 #include <chrono>
 #include <iostream>
 #include <jubatus/msgpack/rpc/session_pool.h>
+#include <fstream>
+#include <sstream>
 #include "rdd_context.h"
 
 using namespace std;
 
+static bool ReadConf(const char *conf_path, vector<pair<string, int>> &slaves) {
+  ifstream ifs(conf_path, ios::in);
+  if (ifs.fail()) {
+    return false;
+  }
+
+  string line, addr, port;
+  while (getline(ifs, line)) {
+    istringstream splitter(line);
+    getline(splitter, addr, ' ');
+    getline(splitter, port, ' ');
+    slaves.push_back(make_pair(addr, stoi(port)));
+  }
+
+  return true;
+}
+
 int main(int argc, const char **argv) {
+  if (argc != 4) {
+    cerr << "Usage: " << argv[0] << "[conf_path] [text_file] [map_reduce.so]" << endl;
+    return 1;
+  }
+
   vector<pair<string, int>> slaves;
-  slaves.push_back(make_pair("127.0.0.1", 50090));
-  slaves.push_back(make_pair("127.0.0.1", 50091));
-  slaves.push_back(make_pair("127.0.0.1", 50092));
-  slaves.push_back(make_pair("127.0.0.1", 50093));
+  if (!ReadConf(argv[1], slaves)) {
+    cerr << "could not read the conf file: " << argv[1] << endl;
+    return 1;
+  }
 
   RddContext rc("localhost", slaves);
   rc.Init();
 
   auto start_text_file = chrono::steady_clock::now();
-  auto textFile = rc.TextFile("/home/hdaikoku/Desktop/shared/512.txt");
+  auto textFile = rc.TextFile(argv[2]);
   auto end_text_file = chrono::steady_clock::now();
 
   auto start_map = chrono::steady_clock::now();
-  auto mapped = textFile->Map("/home/hdaikoku/ClionProjects/rdd/examples/libWordCount.so");
+  auto mapped = textFile->Map(argv[3]);
   auto end_map = chrono::steady_clock::now();
 
   auto start_reduce = chrono::steady_clock::now();
-  auto reduced = mapped->Reduce("/home/hdaikoku/ClionProjects/rdd/examples/libWordCount.so");
+  auto reduced = mapped->Reduce(argv[3]);
   auto end_reduce = chrono::steady_clock::now();
 
   reduced->Print();
 
   cout << "TextFile: "
-      << chrono::duration_cast<chrono::seconds>(end_text_file - start_text_file).count()
+      << chrono::duration_cast<chrono::milliseconds>(end_text_file - start_text_file).count()
       << " s" << endl;
 
   cout << "Map: "
-      << chrono::duration_cast<chrono::seconds>(end_map - start_map).count()
+      << chrono::duration_cast<chrono::milliseconds>(end_map - start_map).count()
       << " s" << endl;
 
   cout << "Reduce: "
-      << chrono::duration_cast<chrono::seconds>(end_reduce - start_reduce).count()
+      << chrono::duration_cast<chrono::milliseconds>(end_reduce - start_reduce).count()
       << " s" << endl;
 
   return 0;
