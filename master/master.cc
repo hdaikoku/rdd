f1708fc693bbcc8810d2b98c8dfc668492cfdba5
Harunobu Daikoku <daikoku@hpcs.cs.tsukuba.ac.jp>
2015-11-18 14:48:51 +0900

    add time measuring codes

--- master/master.cc
+++ master/master.cc
@@ -2,12 +2,12 @@
 // Created by Harunobu Daikoku on 2015/10/26.
 //
 
+#include <chrono>
 #include <iostream>
 #include <jubatus/msgpack/rpc/session_pool.h>
 #include "rdd_context.h"
 
 using namespace std;
-using namespace msgpack;
 
 int main(int argc, const char **argv) {
   vector<pair<string, int>> slaves;
@@ -18,10 +18,32 @@ int main(int argc, const char **argv) {
 
   RddContext rc("localhost", slaves);
   rc.Init();
-  rc.TextFile("/home/hdaikoku/Desktop/shared/512.txt")
-      ->Map("/home/hdaikoku/ClionProjects/rdd/examples/libWordCount.so")
-      ->Reduce("/home/hdaikoku/ClionProjects/rdd/examples/libWordCount.so")
-      ->Print();
+
+  auto start_text_file = chrono::steady_clock::now();
+  auto textFile = rc.TextFile("/home/hdaikoku/Desktop/shared/512.txt");
+  auto end_text_file = chrono::steady_clock::now();
+
+  auto start_map = chrono::steady_clock::now();
+  auto mapped = textFile->Map("/home/hdaikoku/ClionProjects/rdd/examples/libWordCount.so");
+  auto end_map = chrono::steady_clock::now();
+
+  auto start_reduce = chrono::steady_clock::now();
+  auto reduced = mapped->Reduce("/home/hdaikoku/ClionProjects/rdd/examples/libWordCount.so");
+  auto end_reduce = chrono::steady_clock::now();
+
+  reduced->Print();
+
+  cout << "TextFile: "
+      << chrono::duration_cast<chrono::seconds>(end_text_file - start_text_file).count()
+      << " s" << endl;
+
+  cout << "Map: "
+      << chrono::duration_cast<chrono::seconds>(end_map - start_map).count()
+      << " s" << endl;
+
+  cout << "Reduce: "
+      << chrono::duration_cast<chrono::seconds>(end_reduce - start_reduce).count()
+      << " s" << endl;
 
   return 0;
 }
