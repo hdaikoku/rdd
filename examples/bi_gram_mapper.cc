b0000b5ead625a591415abd046e5b1a98b2fb47d
Harunobu Daikoku <daikoku@hpcs.cs.tsukuba.ac.jp>
2016-01-19 00:10:12 +0900

    fastest mappers ever :)

--- examples/bi_gram_mapper.cc
+++ examples/bi_gram_mapper.cc
@@ -7,20 +7,12 @@
 void BiGramMapper::Map(std::unordered_map<std::string, std::vector<int>, tbb::tbb_hash<std::string>> &kvs,
                        const long long int &key,
                        const std::string &value) {
-  size_t cur = 0, pos = 0;
-  std::string word1, word2;
-
-  if ((pos = value.find_first_of(" ", cur)) == std::string::npos) {
-    return;
-  }
-
-  word1 = value.substr(cur, pos - cur);
-  cur = pos + 1;
-
-  while ((pos = value.find_first_of(" ", cur)) != std::string::npos) {
-    word2 = value.substr(cur, pos - cur);
-    kvs[word1 + " " + word2].push_back(1);
-    cur = pos + 1;
-    word1 = word2;
+  size_t cur = 0, p1, p2 = 0;
+  p1 = value.find_first_of(" ", cur);
+  while (p2 != value.npos) {
+    p2 = value.find_first_of(" ", p1 + 1);
+    kvs[value.substr(cur, p2 - cur)].emplace_back(1);
+    cur = p1 + 1;
+    p1 = p2;
   }
 }
